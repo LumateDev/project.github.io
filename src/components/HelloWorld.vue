2528be6ad6f35858a6b1091eb412b572e0519ffc
Alex <Sanchous557@mail.ru>
2022-12-06 08:23:51 +0300

    test commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       Просто какой-то текст, для теста
+      И ещё текст
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
