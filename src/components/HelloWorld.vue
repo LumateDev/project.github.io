262473c1c914c06b644149bf78d6a5745f2730d5
Alex <Sanchous557@mail.ru>
2022-12-07 11:49:10 +0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,6 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       Test comment from work branch
+      NEW UPDATE FROM ALEX!!!!!!
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
