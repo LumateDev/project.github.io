a3c21472f930d3d8053d1e1364f9f77ab0e2a608
mattakvshi <ytube9504@gmail.com>
2022-12-18 14:35:52 +0300

    Исправил ошибку с блоком контеннта, сверстал 2 контейнера, сверстал <head> (иконку, название, новое подключение Botstrap и Botstrap-vue)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,26 @@
 <template>
-  <AppHeader></AppHeader>
-  <AppContent></AppContent>
-  <AppFooter></AppFooter>
-
-
+	<AppHeader></AppHeader>
+	<AppContent></AppContent>
+	<AppFooter></AppFooter>
 </template>
 
 <script>
-import AppHeader from "@/components/AppHeader.vue";
-import AppContent from "@/components/AppContent.vue";
-import AppFooter from "@/components/AppFooter.vue";
+import AppHeader from '@/components/AppHeader.vue'
+import AppContent from '@/components/AppContent.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    AppContent,
-    AppFooter,
-  }
+	name: 'App',
+	components: {
+		AppHeader,
+		AppContent,
+		AppFooter,
+	},
 }
 </script>
 
 <style>
-
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: 'Montserrat', sans-serif;
 }
 </style>
